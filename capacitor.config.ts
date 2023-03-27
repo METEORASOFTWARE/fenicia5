f16eeb00246233238a46670c5b4b728c11cbd712
METEORASOFTWARE <31638608+METEORASOFTWARE@users.noreply.github.com>
2023-03-27 10:39:55 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'fenicia2',
+  webDir: 'dist/fenicia2',
+  bundledWebRuntime: false
+};
+
+export default config;
